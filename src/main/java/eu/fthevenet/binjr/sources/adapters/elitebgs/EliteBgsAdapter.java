bf1af5b73e830ab74d34bd0a7f90af25df684ce0
Frederic Thevenet <thevenet.fred@free.fr>
2019-08-13 17:46:24 +0200

    Make http calls off the UI thread

--- src/main/java/eu/fthevenet/binjr/sources/adapters/elitebgs/EliteBgsAdapter.java
+++ src/main/java/eu/fthevenet/binjr/sources/adapters/elitebgs/EliteBgsAdapter.java
@@ -18,15 +18,20 @@ package eu.fthevenet.binjr.sources.adapters.elitebgs;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import eu.binjr.common.logging.Profiler;
 import eu.binjr.core.data.adapters.HttpDataAdapter;
 import eu.binjr.core.data.adapters.TimeSeriesBinding;
+import eu.binjr.core.data.async.AsyncTaskManager;
 import eu.binjr.core.data.codec.Decoder;
 import eu.binjr.core.data.exceptions.CannotInitializeDataAdapterException;
 import eu.binjr.core.data.exceptions.DataAdapterException;
 import eu.binjr.core.data.workspace.ChartType;
 import eu.binjr.core.data.workspace.UnitPrefixes;
+import eu.binjr.core.dialogs.Dialogs;
 import eu.fthevenet.binjr.sources.adapters.elitebgs.api.v4.EBGSSystemsPageV4;
 import eu.fthevenet.binjr.sources.adapters.elitebgs.api.v4.EBGSSystemsV4;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import org.apache.http.NameValuePair;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.message.BasicNameValuePair;
@@ -44,6 +49,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class EliteBgsAdapter extends HttpDataAdapter {
@@ -51,8 +57,9 @@ public class EliteBgsAdapter extends HttpDataAdapter {
     private static final String SITE = "https://elitebgs.app";
     private static final String API_FACTIONS = "/api/ebgs/v4/factions";
     private static final String API_SYSTEMS = "/api/ebgs/v4/systems";
-    private static final String FRONTEND_FACTIONS = "/frontend/factions";
-    private static final String FRONTEND_SYSTEMS = "/frontend/systems";
+    // private static final String FRONTEND_FACTIONS = "/frontend/factions";
+    //   private static final String FRONTEND_SYSTEMS = "/frontend/systems";
+    private static final String TITLE = "Elite BGS";
 
 
     public EliteBgsAdapter() throws CannotInitializeDataAdapterException {
@@ -85,84 +92,89 @@ public class EliteBgsAdapter extends HttpDataAdapter {
 
     @Override
     public FilterableTreeItem<TimeSeriesBinding> getBindingTree() throws DataAdapterException {
-        var root = new FilterableTreeItem<>(new TimeSeriesBinding(
-                "Elite BGS",
-                "",
-                null,
-                "Elite BGS",
-                UnitPrefixes.METRIC,
-                ChartType.LINE,
-                "",
-                "Elite BGS\\",
-                this));
-        var systems = getSystems();
+        var root = makeBranch(TITLE, TITLE, "\\");
+        var systems = getSystems(root);
         var factions = getFactions();
         root.getInternalChildren().addAll(systems, factions);
 
         return root;
     }
 
-    private FilterableTreeItem<TimeSeriesBinding> getSystems() throws DataAdapterException {
-        Gson gson = new Gson();
-        var tree = new FilterableTreeItem<>(new TimeSeriesBinding(
-                "Systems",
-                "\\Systems\\",
-                null,
-                "Systems",
-                UnitPrefixes.METRIC,
-                ChartType.LINE,
-                "%",
-                "Elite BGS\\Systems",
-                this));
-        for (int i = 1; i < 10; i++) {
-            addSystemsPage(gson, tree, i);
+    private FilterableTreeItem<TimeSeriesBinding> getSystems(FilterableTreeItem<TimeSeriesBinding> root) throws DataAdapterException {
+        var tree = makeBranch("Systems", "Systems", root.getValue().getTreeHierarchy());
+        String alphabet = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < alphabet.length(); i++) {
+            String letter = String.valueOf(alphabet.charAt(i));
+            var newBranch = makeBranch(letter, letter, tree.getValue().getTreeHierarchy());
+            // add a dummy node so that the branch can be expanded
+            newBranch.getInternalChildren().add(new FilterableTreeItem<>(null));
+            // add a listener so that bindings for individual datastore are added lazily to avoid
+            // dozens of individual call to "graphdesc" when the tree is built.
+            newBranch.expandedProperty().addListener(new SystemExpandListener(letter, letter, newBranch, tree));
+//
+            tree.getInternalChildren().add(newBranch);
         }
         return tree;
     }
 
-    private void addSystemsPage(Gson gson, FilterableTreeItem<TimeSeriesBinding> tree, int page) throws DataAdapterException {
-        try {
-            EBGSSystemsPageV4 t = gson.fromJson(getSystemsTree(page), EBGSSystemsPageV4.class);
-            Map<String, EBGSSystemsV4> m = Arrays.stream(t.docs).collect(Collectors.toMap(o -> o._id, (o -> o)));
-            for (EBGSSystemsV4 s : m.values()) {
-                var branch = new FilterableTreeItem<>(new TimeSeriesBinding(
-                        s.name,
-                        s._id,
-                        null,
-                        s.name,
-                        UnitPrefixes.METRIC,
-                        ChartType.LINE,
-                        "%",
-                        tree.getValue().getTreeHierarchy() + "\\" + s.name,
-                        this));
-                for (var f : s.factions) {
-                    branch.getInternalChildren().add(new FilterableTreeItem<>(new TimeSeriesBinding(
-                            f.name,
-                            f.faction_id,
-                            null,
-                            f.name,
-                            UnitPrefixes.METRIC,
-                            ChartType.LINE,
-                            "%",
-                            branch.getValue().getTreeHierarchy() + "\\" + f.name,
-                            this)));
-                }
-                tree.getInternalChildren().add(branch);
+    private void addAllSystemsPages(FilterableTreeItem<TimeSeriesBinding> tree, String beginWith) throws DataAdapterException {
+        AtomicInteger nbPages = new AtomicInteger(0);
+        try (var p = Profiler.start(() -> "Retrieving all systems starting with " + beginWith + " (" + nbPages.get() + " pages)", logger::trace)) {
+            nbPages.addAndGet(addSystemsPage(tree, beginWith, 1));
+            for (int page = 2; page < nbPages.get(); page++) {
+                addSystemsPage(tree, beginWith, page);
             }
-//            return tree;
-        } catch (JsonSyntaxException e) {
-            throw new DataAdapterException("An error occurred while parsing the json response to getBindingTree request", e);
-        } /*catch (URISyntaxException e) {
-            throw new SourceCommunicationException("Error building URI for request", e);
-        }*/
+        }
     }
 
-    private String getSystemsTree(int page) throws DataAdapterException {
+    private int addSystemsPage(FilterableTreeItem<TimeSeriesBinding> tree, String beginWith, int page) throws DataAdapterException {
+        try {
+            Gson gson = new Gson();
+            AsyncTaskManager.getInstance().submit(() -> gson.fromJson(getSystemsTree(page, beginWith), EBGSSystemsPageV4.class),
+                    event -> {
+                        //sourceMaskerPane.setVisible(false);
+                        EBGSSystemsPageV4 t = (EBGSSystemsPageV4) event.getSource().getValue();
+                        Map<String, EBGSSystemsV4> m = Arrays.stream(t.docs).collect(Collectors.toMap(o -> o._id, (o -> o)));
+                        for (EBGSSystemsV4 s : m.values()) {
+                            var branch = makeBranch(s.name, s._id, tree.getValue().getTreeHierarchy());
+                            for (var f : s.factions) {
+                                branch.getInternalChildren().add(makeBranch(f.name, f.faction_id, branch.getValue().getTreeHierarchy()));
+                            }
+                            tree.getInternalChildren().add(branch);
+                        }
+
+                    //    return t.pages;
+                    },
+                    event -> {
+                        //   sourceMaskerPane.setVisible(false);
+                        Dialogs.notifyException("An error occurred while parsing the json response to getBindingTree request",
+                                event.getSource().getException());
+                    });
+            return 10;//t.pages;
+        } catch (JsonSyntaxException e) {
+            throw new DataAdapterException("An error occurred while parsing the json response to getBindingTree request", e);
+        }
+    }
+
+    private FilterableTreeItem<TimeSeriesBinding> makeBranch(String name, String id, String parentHierarchy) {
+        return new FilterableTreeItem<>(new TimeSeriesBinding(
+                name,
+                id,
+                null,
+                name,
+                UnitPrefixes.METRIC,
+                ChartType.LINE,
+                "%",
+                parentHierarchy + "\\" + name,
+                this));
+    }
+
+    private String getSystemsTree(int page, String beginWith) throws DataAdapterException {
         List<NameValuePair> params = new ArrayList<>();
         params.add(new BasicNameValuePair("page", Integer.toString(page)));
-        params.add(new BasicNameValuePair("beginsWith", ""));
+        params.add(new BasicNameValuePair("beginsWith", beginWith));
 
-        String entityString = doHttpGet(craftRequestUri(FRONTEND_SYSTEMS, params), new BasicResponseHandler());
+        String entityString = doHttpGet(craftRequestUri(API_SYSTEMS, params), new BasicResponseHandler());
         logger.trace(entityString);
         return entityString;
     }
@@ -195,5 +207,34 @@ public class EliteBgsAdapter extends HttpDataAdapter {
     @Override
     public String getSourceName() {
         return "[Elite BGS]";
+    }
+
+    private class SystemExpandListener implements ChangeListener<Boolean> {
+        private final String currentPath;
+        private final FilterableTreeItem<TimeSeriesBinding> newBranch;
+        private final FilterableTreeItem<TimeSeriesBinding> tree;
+        private final String beginWith;
+
+        public SystemExpandListener(String currentPath, String beginWith, FilterableTreeItem<TimeSeriesBinding> newBranch, FilterableTreeItem<TimeSeriesBinding> tree) {
+            this.currentPath = currentPath;
+            this.newBranch = newBranch;
+            this.beginWith = beginWith;
+            this.tree = tree;
+        }
+
+        @Override
+        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            if (newValue) {
+                try {
+                    addAllSystemsPages(newBranch, beginWith);
+                    //remove dummy node
+                    newBranch.getInternalChildren().remove(0);
+                    // remove the listener so it isn't executed next time node is expanded
+                    newBranch.expandedProperty().removeListener(this);
+                } catch (Exception e) {
+                    Dialogs.notifyException("Failed to retrieve graph description", e);
+                }
+            }
+        }
     }
 }
